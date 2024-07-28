23018ae871bd6939a6387ad3cb2fd6cffb2f4c01
SteliosKem <stylianoskem@gmail.com>
2024-07-28 23:01:02 +0300

    Added GLFW

--- Ivory/src/Application.cpp
+++ Ivory/src/Application.cpp
@@ -1,13 +1,16 @@
 #include "pch.h"
 #include "Application.h"
 #include "WindowEvent.h"
+#include "KeyEvent.h"
 #include "Log.h"
 
 namespace Ivory {
 	void Application::run() {
 		WindowResizeEvent e(1280, 720);
+		KeyPressedEvent _e(2, 1);
 
 		IV_TRACE(e.to_string());
+		IV_WARN(_e.to_string());
 
 		while (true) {
 
