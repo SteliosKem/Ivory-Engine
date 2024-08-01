de8f87a0f826d1d8a14d8b36cf48a73d667bb2d4
SteliosKem <stylianoskem@gmail.com>
2024-08-01 12:20:30 +0300

    Update IvoryEngine.h

--- Ivory/src/IvoryEngine.h
+++ Ivory/src/IvoryEngine.h
@@ -1,10 +1,12 @@
 #pragma once
 
-
+using Real = float;
 // USE ONLY IN EXTERNAL APPLICATIONS
 #include "Application.h"
 #include "Layers/Layer.h"
 #include "Logging/Log.h"
 #include "ImGui/ImGuiLayer.h"
+#include "IMath.h"
+#include "Input.h"
 
 #include "Entry.h"
